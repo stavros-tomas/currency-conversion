eb59de5a781051be4721f72def72ef85f560c411
St tomas <hello@stavrostomas.eu>
2024-11-17 11:15:17 +0000

    currency-conversion:feat:main app view

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
-import Button from '@mui/material/Button';
+import Convert from './components/Convert/Convert'
 
 function App() {
   return (
-    <>
-      <h1>Currency Conversion</h1>
-      <Button variant="contained">Convert</Button>
-    </>
+    <div className="tw-flex tw-flex-col tw-items-center tw-p-4">
+      <h1 className="tw-text-3xl tw-font-bold">Currency Conversion</h1>
+
+      <div className="tw-mt-10 tw-w-full tw-max-w-xl">
+        <Convert/>
+      </div>
+    </div>
   )
 }
 
